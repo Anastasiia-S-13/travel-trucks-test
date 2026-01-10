c3090617bbbbfd0b7bec44d8b2e28d7f08ca9ca4
Anastasiia <gorgona1597531597@gmail.com>
2026-01-10 21:07:03 +0100

    add loading and campers page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
       <p>Travel Trucks</p>
+      <Link href="/campers">Campers</Link>
     </header>
   );
 }
---
+++ types/Camper.ts
@@ -0,0 +1,50 @@
+export interface Camper {
+  id: string;
+  name: string;
+  price: number;
+  rating: number;
+  location: string;
+  description: string;
+
+  form: "alcove" | "integrated" | "semi-integrated" | string;
+
+  length: string;
+  width: string;
+  height: string;
+
+  tank: string;
+  consumption: string;
+
+  transmission: "automatic" | "manual";
+  engine: "diesel" | "petrol" | "electric" | string;
+
+  AC: boolean;
+  bathroom: boolean;
+  kitchen: boolean;
+  TV: boolean;
+  radio: boolean;
+  refrigerator: boolean;
+  microwave: boolean;
+  gas: boolean;
+  water: boolean;
+
+  gallery: CamperImage[];
+  reviews: CamperReview[];
+}
+
+export interface CamperImage {
+  thumb: string;
+  original: string;
+}
+
+export interface CamperReview {
+  reviewer_name: string;
+  reviewer_rating: number;
+  comment: string;
+}
+
+export interface CampersResponse {
+  campers: Camper[];
+  page: number;
+  totalPages: number;
+}
