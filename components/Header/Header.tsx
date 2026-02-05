ad3f35dd4b8084aca977be3579ef196cbf3e5f16
Anastasiia <gorgona1597531597@gmail.com>
2026-02-05 21:05:02 +0100

    add header css

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import Image from "next/image";
 import Link from "next/link";
+import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { href: "/", label: "Home" },
+    { href: "/campers", label: "Catalog" },
+  ];
   return (
-    <header>
-      <p>Travel Trucks</p>
-      <Link href="/campers">Campers</Link>
+    <header className={css.headerContainer}>
+      <Image src="/logo.svg" alt="Travel Trucks Logo" width={136} height={16} />
+      <nav className={css.navContainer}>
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href;
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`${css.link} ${isActive ? css.active : ""}`}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </nav>
     </header>
   );
 }
