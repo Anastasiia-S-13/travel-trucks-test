837f06c6b47deb9377ff2eb2065369504ee1f18f
Anastasiia <gorgona1597531597@gmail.com>
2026-02-10 12:08:09 +0100

    add styles for camper's list

--- app/(site)/campers/CampersPage.Client.tsx
+++ app/(site)/campers/CampersPage.Client.tsx
@@ -5,32 +5,33 @@ import { Toaster } from "react-hot-toast";
 import { fetchCampers } from "@/lib/api/campers";
 import CampersCatalog from "@/components/Campers/CampersCatalog";
 import { CampersResponse } from "@/types/Camper";
+import css from "./CampersPage.module.css";
 
 export default function CampersPageClient() {
-   const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isLoading,
-    isError,
-} = useInfiniteQuery<
-    CampersResponse,                           // TQueryFnData
-    Error,                                     // TError
-    InfiniteData<CampersResponse, number>,     // ✅ TData
-    ["campers"],                               // TQueryKey
-    number                                     // TPageParam
->({
-    queryKey: ["campers"],
-    initialPageParam: 1,
-    queryFn: ({ pageParam }) => fetchCampers(pageParam),
-    getNextPageParam: (lastPage) => {
-        if (lastPage.campers.length < 4) {
-            return undefined;
-        }
-        return lastPage.page + 1;
-    },
-});
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        isLoading,
+        isError,
+    } = useInfiniteQuery<
+        CampersResponse,                           // TQueryFnData
+        Error,                                     // TError
+        InfiniteData<CampersResponse, number>,     // ✅ TData
+        ["campers"],                               // TQueryKey
+        number                                     // TPageParam
+    >({
+        queryKey: ["campers"],
+        initialPageParam: 1,
+        queryFn: ({ pageParam }) => fetchCampers(pageParam),
+        getNextPageParam: (lastPage) => {
+            if (lastPage.campers.length < 4) {
+                return undefined;
+            }
+            return lastPage.page + 1;
+        },
+    });
 
     const campers = data?.pages.flatMap((page) => page.campers) ?? [];
 
@@ -38,7 +39,7 @@ export default function CampersPageClient() {
     if (isError) return <p>Error loading campers</p>;
 
     return (
-        <div>
+        <div className="container">
             <Toaster />
 
             {campers.length > 0 ? (
--- components/Campers/CampersCatalog.tsx
+++ components/Campers/CampersCatalog.tsx
@@ -2,28 +2,33 @@ import { Camper } from "@/types/Camper";
 import Image from "next/image";
 import Link from "next/link";
 import CamperEquipments from "./CampersEquipments/CampersEquipments";
+import css from "./CampersCatalog.module.css";
 
 export default function CampersCatalog({ campers }: { campers: Camper[] }) {
   return (
-    <ul>
+    <ul className={css.camperList}>
       {campers.map((camper, index) => (
-        <li key={camper._id || index}>
-         <Image
+        <li className={css.camperItem} key={camper._id || index}>
+          <Image
+            className={css.camperImage}
             src={camper.gallery[0]?.thumb}
             alt="Camper truck"
             width={292}
             height={320}
           />
-          <p>€{camper.price}</p>
-          <p>{camper.rating}</p>
-          <p>{camper.location}</p>
-          <p>{camper.description}</p>
-          <ul>
-            <li>{camper.transmission}</li>
-            <li>{camper.engine}</li>
-            <CamperEquipments camper={camper} />
-          </ul>
-          <Link href={`/campers/${camper._id}`}>Show more</Link>
+          <div className={css.camperDetails}>
+            <h2 className={css.camperName}>{camper.name}</h2>
+            <p className={css.camperPrice}>€{camper.price}</p>
+            <p>{camper.rating}({camper.reviews.length} Reviews)</p>
+            <p>{camper.location}</p>
+            <p>{camper.description}</p>
+            <ul>
+              <li>{camper.transmission}</li>
+              <li>{camper.engine}</li>
+              <CamperEquipments camper={camper} />
+            </ul>
+            <Link href={`/campers/${camper._id}`}>Show more</Link>
+          </div>
         </li>
       ))}
     </ul>
