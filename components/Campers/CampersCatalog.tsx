2aea97b38d697caf1997574af5908a83de8483c3
Anastasiia <gorgona1597531597@gmail.com>
2026-02-12 12:18:52 +0100

    add styles for descriptions

--- components/Campers/CampersCatalog.tsx
+++ components/Campers/CampersCatalog.tsx
@@ -5,28 +5,35 @@ import CamperEquipments from "./CampersEquipments/CampersEquipments";
 import css from "./CampersCatalog.module.css";
 
 export default function CampersCatalog({ campers }: { campers: Camper[] }) {
+  const maxLength =60;
   return (
     <ul className={css.camperList}>
-      {campers.map((camper, index) => (
-        <li className={css.camperItem} key={camper._id || index}>
-          <Image
-            className={css.camperImage}
-            src={camper.gallery[0]?.thumb}
-            alt="Camper truck"
-            width={292}
-            height={320}
-          />
-          <div className={css.camperDetails}>
-            <h2 className={css.camperName}>{camper.name}</h2>
-            <p className={css.camperPrice}>€{camper.price}</p>
-            <p>{camper.rating}({camper.reviews.length} Reviews)</p>
-            <p>{camper.location}</p>
-            <p>{camper.description}</p>
+      {campers.map((camper, index) => {
+        const truncatedDescription = camper.description.length > maxLength
+          ? camper.description.substring(0, maxLength) + "..."
+          : camper.description;
+
+        return (
+          <li className={css.camperItem} key={camper._id || index}>
+            <Image
+              className={css.camperImage}
+              src={camper.gallery[0]?.thumb}
+              alt="Camper truck"
+              width={292}
+              height={320}
+            />
+            <div className={css.camperDetails}>
+              <h2 className={css.camperName}>{camper.name}</h2>
+              <p className={css.camperPrice}>€{camper.price}</p>
+              <p>{camper.rating}({camper.reviews.length} Reviews)</p>
+              <p>{camper.location}</p>
+              <p className={css.camperDescription}>{truncatedDescription}</p>
               <CamperEquipments camper={camper} />
-            <Link className={css.showMoreLink} href={`/campers/${camper._id}`}>Show more</Link>
-          </div>
-        </li>
-      ))}
+              <Link className={css.showMoreLink} href={`/campers/${camper._id}`}>Show more</Link>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
